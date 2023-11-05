574c5e74f910282c7a9cc6e6833f0660512cf99e
samuelsm <samuelmeurer13@gmail.com>
2023-11-05 17:52:43 -0300

    adicionando teste com hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+import { useWeb3Modal } from '@web3modal/wagmi/react'
+import { useAccount } from 'wagmi'
+import ContratoABI from './contractAbi.json';
+import { WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet } from 'wagmi/chains'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  useWeb3ModalEvents,
+  useWeb3ModalState,
+  useWeb3ModalTheme
+} from '@web3modal/wagmi/react'
+
+
+// 1. Get projectId
+const projectId = '7c755aa5bd96e9a1625834900865fbe0'
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-export default App;
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains })
+
+
+function Apps() {
+  const account = useAccount({
+    onConnect({ address, connector, isReconnected }) {
+      console.log('Connected', { address, connector, isReconnected })
+    },
+  })
+}
+
+
+
+
+export default function App() {
+  // 4. Use modal hook
+  const modal = useWeb3Modal()
+  const state = useWeb3ModalState()
+  const { themeMode, themeVariables, setThemeMode } = useWeb3ModalTheme()
+  const events = useWeb3ModalEvents()
+
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <w3m-button />
+      <w3m-network-button />
+      <w3m-connect-button />
+      <w3m-account-button />
+
+      <button onClick={() => modal.open()}>Connect Wallet</button>
+      <button onClick={() => modal.open({ view: 'Networks' })}>Choose Network</button>
+      <button onClick={() => setThemeMode(themeMode === 'dark' ? 'light' : 'dark')}>
+        Toggle Theme Mode
+      </button>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <pre>{JSON.stringify({ themeMode, themeVariables }, null, 2)}</pre>
+      <pre>{JSON.stringify(events, null, 2)}</pre>
+    </WagmiConfig>
+  )
+}
